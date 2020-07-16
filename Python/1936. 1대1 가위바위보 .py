499ca5456ea230516831f1bc19a174d683344247
오승환 <rayoh95@naver.com>
2020-07-17 01:25:46 +0900

    200716_Python

---
+++ Python/1936. 1대1 가위바위보 .py
@@ -0,0 +1,38 @@
+A, B = map(int, input().split())
+
+# 1 = 가위, 2 = 바위, 3 = 보
+if A == 1:
+    if B == 2:
+        print("B")
+    else:
+        print("A")
+elif A == 2:
+    if B == 1:
+        print("A")
+    else:
+        print("B")
+else:
+    if B == 1:
+        print("B")
+    else:
+        print("A")
+
+"""
+if A == 1 and B == 2:
+    print("B")
+
+elif A == 2 and B == 1:
+    print("A")
+
+elif A == 1 and B == 3:
+    print("A")
+
+elif A == 3 and B == 1:
+    print("B")
+
+elif A == 2 and B == 3:
+    print("B")
+
+else:
+    print("A")
+"""
