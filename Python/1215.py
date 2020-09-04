87603c2de5b93a711e28a3d86bc6fd451b349a81
오승환 <rayoh95@naver.com>
2020-09-04 17:28:23 +0900

    * 2차원 리스트 (200904)
    ** for 문과 if 문을 적절히 이용하여 2차원 리스트에 넣는다.
    ** 2차원 리스트에 리스트 추출 함수를 사용 할 수 있다는 점을 알자

---
+++ Python/1215.py
@@ -0,0 +1,17 @@
+# 회문1
+
+for test in range(1, 11):
+    N = int(input())
+    arr = [input() for i in range(8)]   
+    count = 0
+    for i in range(8):
+        temp = ''
+        for j in range(8):
+            temp += arr[j][i]
+        for r in range(8-N+1):
+            if arr[i][r:r+N] == arr[i][r:r+N][::-1]:
+                count += 1
+            if temp[r:r+N] == temp[r:r+N][::-1]:
+                count += 1
+ 
+    print(f'#{test+1} {count}')
