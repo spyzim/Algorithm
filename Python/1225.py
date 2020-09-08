7fbd49e1963c2e8fe42e3c0c831c15fbc5b1624e
오승환 <rayoh95@naver.com>
2020-09-08 09:41:03 +0900

    * break 문의 위치 (200907)
    ** break 의 위치 확인: break 는 바로 위의 반복문을 제어한다
    ** 너무 함수 사용에 매몰되지 말자. 안되면 그냥 풀 것

--- Python/1225.py
+++ Python/1225.py
@@ -1,27 +1,37 @@
 # 암호생서기
 
 # 리스트를 받아 첫번째 요소에 n 을 뺀 후 맨 뒤로 이동시키는 함수. 
-def coding(list1, n, judge):
-    list1[0] -= n
-    if list1[0] <= 0:
-        list1[0] = 0
-        judge = 0
-    temp = list1[0]
+# def coding(list1, n, j):
+#     list1[0] -= n
+#     if list1[0] <= 0:
+#         list1[0] =0
+#         j -= j
+#     temp = list1[0]
 
-    for i in range(7):
-        list1[i] = list1[i+1]
-    list1[7] = temp
+#     for i in range(7):
+#         list1[i] = list1[i+1]
+#     list1[7] = temp
 
-for case in range(2):
+for case in range(10):
     case_num = int(input())
     code = list(map(int, input().split()))
     judge = 1
     while True:
-        for i in range(1,5):
-            coding(code, i, judge)
-            if judge == 0:
+        for i in range(1,6):
+            code[0] -= i
+            if code[0] <= 0:
+                code[0] = 0
+            temp = code[0]
+
+            for j in range(7):
+                code[j] = code[j+1]
+            code[7] = temp
+
+            if code[7] == 0:
                 break
-    print(f'{case_num + 1}', end = " ")
+        if code[7] == 0:
+            break
+    print(f'#{case_num}', end = " ")
     for i in range(8):
         print(code[i], end = " ")
     print()
