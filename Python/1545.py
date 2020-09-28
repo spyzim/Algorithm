9dbeb5dc7b865cd5ab7d292663b5efa7403a931b
오승환 <rayoh95@naver.com>
2020-09-28 10:29:11 +0900

    200928_Python

---
+++ Python/1545.py
@@ -0,0 +1,7 @@
+# 거꾸로 출력해 보아요
+
+n = int(input())
+
+for i in range(n):
+    print(n-i, end = ' ')
+print(0)
---
+++ Python/1979.py
@@ -0,0 +1,7 @@
+# 어디에 단어가 들어갈 수 있을까
+
+test = int(input())
+
+for case in range(test):
+    N, K = map(int, input().split())
+    
